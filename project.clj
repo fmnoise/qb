f4c3d47262fdb24261ef50a66480535290f2e791
fmnoise <fmnoise@gmail.com>
2023-01-25 17:35:34 +0100

    Remove repl-options

--- project.clj
+++ project.clj
@@ -3,5 +3,4 @@
   "https://github.com/fmnoise/qb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
-  :repl-options {:init-ns fmnoise.qb})
+  :dependencies [[org.clojure/clojure "1.10.0"]])
